517ed501bb52f0bfaefee6f4ad08ed2e977794eb
Juam Lopez <jl3steban@gmail.com>
2025-11-19 13:25:27 -0500

    Añadiendo Informe

--- pages/informe.py
+++ pages/informe.py
@@ -12,7 +12,7 @@ st.markdown("""
         body{
             background-color: #0e1117;
             color: #EAEAEA;
-        }""", unsafe_allow_html=Truei)
+        }""", unsafe_allow_html=True)
 
 
 
